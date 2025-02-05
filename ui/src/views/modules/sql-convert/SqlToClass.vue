a08db5ad3a82034506458961ae3445d1b0bc12e1
shiloh <lixiaolei595@gmail.com>
2025-02-05 08:38:01 +0800

    feat: change route config

---
+++ ui/src/views/modules/sql-convert/SqlToClass.vue
@@ -0,0 +1,18 @@
+<!--
+  * sql to class
+  * @author shiloh
+  * @date 2025/2/4 21:53
+-->
+<template>
+  <CodeEditor v-model="code" language="sql" />
+</template>
+
+<script setup lang="ts">
+import CodeEditor from 'components/CodeEditor.vue'
+
+const code = ref<string>(
+  'create table T_Users(id bigint primary key, username varchar(50) not null, address nvarchar(255) not null, gender char(1), enabled bit(1))',
+)
+</script>
+
+<style scoped></style>
