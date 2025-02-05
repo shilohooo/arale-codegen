86cdac5b140e96c7412e421459343c5d37696ac0
shiloh <lixiaolei595@gmail.com>
2025-02-05 22:57:38 +0800

    build: set public path

--- ui/quasar.config.ts
+++ ui/quasar.config.ts
@@ -27,6 +27,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
+      publicPath: '/arale-codegen-ui/',
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
