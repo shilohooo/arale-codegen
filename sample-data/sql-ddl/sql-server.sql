2b36aac9014ed156ca00d36002d2256760aebaaa
shiloh <lixiaolei595@gmail.com>
2025-02-11 22:23:57 +0800

    refactor: add sample data

---
+++ sample-data/sql-ddl/sql-server.sql
@@ -0,0 +1,27 @@
+CREATE TABLE T_SqlServerGenerateTest
+(
+    BitCol              bit              not null,
+    TinyIntCol          tinyint          not null,
+    SmallIntCol         smallint         not null,
+    IntCol              int              not null,
+    RealCol             real             not null,
+    BigIntCol           bigint           not null,
+    FloatCol            float            not null,
+    NCharCol            nchar(10)        not null,
+    NVarCharCol         nvarchar(10)     not null,
+    BinaryCol           binary(10)       not null,
+    VarBinaryCol        varbinary(10)    not null,
+    UniqueIdentifierCol uniqueidentifier not null,
+    CharCol             char(10)         not null,
+    VarCharCol          varchar(10)      not null,
+    VarCharMaxCol       varchar(max)     not null,
+    DateCol             date             not null,
+    TimeCol             time             not null,
+    NumericCol          numeric(10, 2)   not null,
+    DecimalCol          decimal(10, 2)   not null,
+    MoneyCol            money,
+    SmallMoneyCol       smallmoney,
+    SmallDatetimeCol    smalldatetime,
+    DatetimeCol         datetime,
+    Datetime2Col        datetime2
+);
